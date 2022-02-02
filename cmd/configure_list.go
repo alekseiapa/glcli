41d0d0ced4af662d6e9ce343a63bb4db93bd2e08
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2022-02-02 20:03:11 +0800

    refactor(config): encapsulate config management into a Config struct
    - Introduce Config struct to encapsulate configuration management.
    - Replace global viper instance with a Config method.
    - Update command functions to use the new Config struct.
    - Adjust tests to align with the new Config structure and usage.
    - Remove obsolete imports and streamline error handling in command execution.

--- cmd/configure_list.go
+++ cmd/configure_list.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"os"
 
-	"github.com/alekseiapa/glcli/internal/config"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +11,7 @@ var configureListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all configuration",
 	Run: func(cmd *cobra.Command, args []string) {
-		config.List(os.Stdout)
+		c.List(os.Stdout)
 	},
 }
 
