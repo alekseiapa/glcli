a0a3ebc5f7dd307b1b9239cdc24c61389818f71b
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2022-02-23 15:02:36 +0800

    refactor(config): Rename edit command to init and update related functions
    - Replace the edit command with init in the configuration CLI tool.
    - Update the short description of the config command to reflect the change.
    - Remove config_edit.go file and create config_init.go to define the init command.
    - Modify the initConfig function to use the new Init method for configuration setup.
    - Rename Edit method to Init in the Config struct, updating its functionality to create or update config files.
    - Update test cases to reflect the method name change from Edit to Init.

--- cmd/config.go
+++ cmd/config.go
@@ -6,7 +6,7 @@ import (
 
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "List or edit glcli CLI options",
+	Short: "Init or list glcli CLI options",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Help()
 	},
